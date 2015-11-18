22bd54061e79aae046b1ab1fa8aa122c438acd45
yyamakawa41 <yyamakawa@gmail.com>
2015-11-18 14:35:13 -0500

    styling
    adding style and images

--- js/controller.js
+++ js/controller.js
@@ -7,15 +7,31 @@ stocksApp.controller('stocksController', function ($scope, $http){
 		var encodedTickers = encodeURIComponent($scope.userStocks);
 		url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20("'+encodedTickers+'")%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
 	
-		$http.get(url).success(function (stockData){
-			// console.log(stockData);
-			// console.log(encodedTickers);
-			$scope.listOfStocks = stockData.query.results.quote;
-			console.log(stockData.query.results)
+		// $http.get(url).success(function (stockData){
+		// 	// console.log(stockData);
+		// 	// console.log(encodedTickers);
+		// 	$scope.listOfStocks = stockData.query.results.quote;
+		// 	console.log(stockData.query.results)
 
-			$scope.listOfStocks = stockData.query.results.quote;
+			$scope.listOfStocks = [
+            {
+                Symbol: "Goog",
+                Change: "+1.2",
+                Name: "Google"
+            },
+            {
+                Symbol: "Yhoo",
+                Change: "-1.2",
+                Name: "Yahoo"
+            },
+            {
+                Symbol: "X",
+                Change: "+2.2",
+                Name: "Trinity Trains"
+            }
+            ];
 
-		});
+		// });
 
 	};
 
@@ -29,4 +45,28 @@ stocksApp.controller('stocksController', function ($scope, $http){
 		}
 	}
 
+	$scope.getChangeClass = function (change){
+		if(change.indexOf('+') > -1){
+			return 'change-positive';
+		}else if(change.indexOf('-') > -1){
+			return 'change-negative';
+		}
+	}
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
