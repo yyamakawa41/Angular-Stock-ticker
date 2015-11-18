c1e3f65ef2031ef1a9101734604d8898008e399a
yyamakawa41 <yyamakawa@gmail.com>
2015-11-18 12:59:25 -0500

    adding click
    added left and middle stock info and click feature to show

--- js/controller.js
+++ js/controller.js
@@ -5,5 +5,28 @@ stocksApp.controller('stocksController', function ($scope, $http){
 
 	$scope.getStocks = function(){
 		var encodedTickers = encodeURIComponent($scope.userStocks);
+		url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20("'+encodedTickers+'")%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
+	
+		$http.get(url).success(function (stockData){
+			// console.log(stockData);
+			// console.log(encodedTickers);
+			$scope.listOfStocks = stockData.query.results.quote;
+			console.log(stockData.query.results)
+
+			$scope.listOfStocks = stockData.query.results.quote;
+
+		});
+
 	};
+
+	$scope.loadStock = function(stockData){
+		$scope.dataList = [];
+		for (name in stockData){
+			$scope.dataList.push({
+				prop: name,
+				value: stockData[name]
+			})
+		}
+	}
+
 });
